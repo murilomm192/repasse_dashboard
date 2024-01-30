15e80c868e7f0835089a759a70b589ee1c44a42d
Murilo <murilomm192@gmail.com>
2024-01-30 18:44:03 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+
+<h1 class="p-1 text-3xl text-red-500 font-bold underline">
+    Hello world!
+</h1>
+
+<p class="text-red-500 text-center text-2xl">
+    testando tailwind
+</p>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
